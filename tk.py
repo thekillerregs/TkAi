9bc63434362aaa64846dda7b45cba0897789be3a
thekillerregs <thekillerregs@gmail.com>
2023-12-25 15:38:56 -0300

    Dot Product

--- tk.py
+++ tk.py
@@ -1,13 +1,19 @@
 import numpy as np
 import torch
 
-# Transposing matrixes
+# Dot product
 
 # Numpy
-nM = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
-print(nM), print(' '), print(nM.T), print(' '), print(nM.T.T)
+nv1 = np.array([1, 2, 3, 4])
+nv2 = np.array([0, 1, 0, -1])
+
+print(np.dot(nv1, nv2))
+
+print(np.sum(nv1 * nv2))
 
 # PyTorch
-tv = torch.tensor([[1, 2, 3, 4]])
-print(tv), print(' '), print(tv.T), print(' '), print(tv.T.T)
+tv1 = torch.tensor([1, 2, 3, 4])
+tv2 = torch.tensor([0, 1, 0, -1])
 
+print(torch.dot(tv1, tv2))
+print(torch.sum(tv1 * tv2))
