eb78f43a04aeaae01b70f3ad0a25343854ddff36
thekillerregs <thekillerregs@gmail.com>
2023-12-31 16:58:09 -0300

    Min/Max, argmin/argmax

--- tk.py
+++ tk.py
@@ -3,54 +3,55 @@ import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-# Entropy & Cross-Entropy
+# Numpy
 
-p = .25
+v = np.array([1, 40, 2, -3])
 
-H = - (p * np.log(p))
-print('Wrong Entropy: ' + str(H))
+minval = np.min(v)
+maxval = np.max(v)
 
-x = [.25, .75]
+print('Min, max: %g, %g' % (minval, maxval))
 
-H = 0
+minidx = np.argmin(v)
+maxidx = np.argmax(v)
 
-for p in x:
-    H += -(p * np.log(p))
+print('Min, max indices: %g,%g' % (minidx, maxidx))
 
-print('Correct Entropy: ' + str(H))
+# Martix
 
-p = .25
+M = np.array([[0, 1, 10], [20, 8, 5]])
 
-# Binary Entropy
-H = -(p * np.log(p) + (1 - p) * np.log(1 - p))
-print('Binary Entropy: ' + str(H))
+print(M, ' ')
 
-# Cross-Entropy
-p = [1, 0]  # sum=1
-q = [.25, .75]  # sum=1
+minvals1 = np.min(M)  # Whole matrix
+minvals2 = np.min(M, axis=0)  # Each column (across rows)
+minvals3 = np.min(M, axis=1)  # Each row ( across columns)
 
-H = 0
-
-for i in range(len(p)):
-    H -= p[i] * np.log(q[i])
-
-print('Cross-Entropy: ' + str(H))
-
-# Binary Cross-Entropy
-H = -(p[0] * np.log(q[0]) + p[1] * np.log(q[1]))
-
-print('Binary Cross-Entropy: ' + str(H))
-
-# Simplification
-H = -np.log(q[0])
-print('Manually Simplified:' + str(H))
+print(minvals1)
+print(minvals2)
+print(minvals3)
 
 # Pytorch
-p_tensor = torch.Tensor(p)
-q_tensor = torch.Tensor(q)
 
-# P = Category Labels
-# Q = Model Probability
-x = F.binary_cross_entropy(q_tensor, p_tensor)
+v = torch.tensor([1, 40, 2, -3])
 
-print(x)
+minval = torch.min(v)
+maxval = torch.max(v)
+
+print('Min, max: %g, %g' % (minval, maxval))
+
+minidx = torch.argmin(v)
+maxidx = torch.argmax(v)
+
+print('Min, max indices: %g, %g' % (minidx, maxidx))
+
+M = torch.tensor([[0, 1, 10], [20, 8, 5]])
+print(M), print(' ')
+
+min1 = torch.min(M)
+
+min2 = torch.min(M, axis=0)
+min3 = torch.min(M, axis=1)
+
+print(min2.values)
+print(min2.indices)
