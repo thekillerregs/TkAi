c20204fb1e020ea71655fa711bdc0894c6d68e93
thekillerregs <thekillerregs@gmail.com>
2023-12-31 17:08:32 -0300

    Mean and Variance

--- tk.py
+++ tk.py
@@ -3,55 +3,21 @@ import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-# Numpy
+# Mean and Variance
 
-v = np.array([1, 40, 2, -3])
+x = [1, 2, 4, 6, 5, 4, 0]
 
-minval = np.min(v)
-maxval = np.max(v)
+n = len(x)
 
-print('Min, max: %g, %g' % (minval, maxval))
+mean1 = np.mean(x)
+mean2 = np.sum(x) / n
 
-minidx = np.argmin(v)
-maxidx = np.argmax(v)
+print(mean1, mean2)
 
-print('Min, max indices: %g,%g' % (minidx, maxidx))
+# Degree of Freedom!
+# ddof default is 0, so in the formula below it would be the equivalent of (n-0) on that parameter...
+# 0 is biased, 1 is unbiased
+var1 = np.var(x, ddof=1)
+var2 = (1 / (n - 1)) * np.sum((x - mean1) ** 2)
 
-# Martix
-
-M = np.array([[0, 1, 10], [20, 8, 5]])
-
-print(M, ' ')
-
-minvals1 = np.min(M)  # Whole matrix
-minvals2 = np.min(M, axis=0)  # Each column (across rows)
-minvals3 = np.min(M, axis=1)  # Each row ( across columns)
-
-print(minvals1)
-print(minvals2)
-print(minvals3)
-
-# Pytorch
-
-v = torch.tensor([1, 40, 2, -3])
-
-minval = torch.min(v)
-maxval = torch.max(v)
-
-print('Min, max: %g, %g' % (minval, maxval))
-
-minidx = torch.argmin(v)
-maxidx = torch.argmax(v)
-
-print('Min, max indices: %g, %g' % (minidx, maxidx))
-
-M = torch.tensor([[0, 1, 10], [20, 8, 5]])
-print(M), print(' ')
-
-min1 = torch.min(M)
-
-min2 = torch.min(M, axis=0)
-min3 = torch.min(M, axis=1)
-
-print(min2.values)
-print(min2.indices)
+print(var1, var2)
