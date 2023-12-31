57d42dc070f3b6525e27ed17410a425e0335c256
thekillerregs <thekillerregs@gmail.com>
2023-12-31 16:45:13 -0300

    Entropy and Cross-Entropy

--- tk.py
+++ tk.py
@@ -1,33 +1,56 @@
 import numpy as np
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-# Logarithms
-x = np.linspace(.0001, 1, 200)
+# Entropy & Cross-Entropy
 
-logx = np.log(x)
+p = .25
 
-fig = plt.figure(figsize=(10, 4))
+H = - (p * np.log(p))
+print('Wrong Entropy: ' + str(H))
 
-plt.rcParams.update({'font.size': 15})
+x = [.25, .75]
 
-plt.plot(x, logx, 'ks-', markerfacecolor='w')
-plt.xlabel('x')
-plt.ylabel('log(x)')
+H = 0
 
-plt.show()
+for p in x:
+    H += -(p * np.log(p))
 
-x = np.linspace(.0001, 1, 200)
+print('Correct Entropy: ' + str(H))
 
-logx = np.log(x)
-expx = np.exp(x)
+p = .25
 
-plt.plot(x, x, color=[.8, .8, .8])
-plt.plot(x, np.exp(logx), 'o', markersize=8)
-plt.plot(x, np.log(expx), 'x', markersize=8)
-plt.xlabel('x')
-plt.ylabel('f(g(x))')
-plt.legend(['unity', 'exp(log(x))', 'log(exp(x))'])
+# Binary Entropy
+H = -(p * np.log(p) + (1 - p) * np.log(1 - p))
+print('Binary Entropy: ' + str(H))
 
-plt.show()
+# Cross-Entropy
+p = [1, 0]  # sum=1
+q = [.25, .75]  # sum=1
+
+H = 0
+
+for i in range(len(p)):
+    H -= p[i] * np.log(q[i])
+
+print('Cross-Entropy: ' + str(H))
+
+# Binary Cross-Entropy
+H = -(p[0] * np.log(q[0]) + p[1] * np.log(q[1]))
+
+print('Binary Cross-Entropy: ' + str(H))
+
+# Simplification
+H = -np.log(q[0])
+print('Manually Simplified:' + str(H))
+
+# Pytorch
+p_tensor = torch.Tensor(p)
+q_tensor = torch.Tensor(q)
+
+# P = Category Labels
+# Q = Model Probability
+x = F.binary_cross_entropy(q_tensor, p_tensor)
+
+print(x)
