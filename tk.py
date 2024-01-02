a5d7880ef25d7a067e04976321611dd0a4a5db5a
thekillerregs <thekillerregs@gmail.com>
2024-01-02 10:19:08 -0300

    Random Sampling and Variability

--- tk.py
+++ tk.py
@@ -1,23 +1,28 @@
 import numpy as np
-import torch
-import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-# Mean and Variance
-
-x = [1, 2, 4, 6, 5, 4, 0]
+x = [1,2,4,6,5,4,0,-5,5,-2,6,10,-9,1,3,-6]
 
 n = len(x)
 
-mean1 = np.mean(x)
-mean2 = np.sum(x) / n
+popmean = np.mean(x)
 
-print(mean1, mean2)
+sample = np.random.choice(x,size=5,replace=True)
+sampmean = np.mean(sample)
 
-# Degree of Freedom!
-# ddof default is 0, so in the formula below it would be the equivalent of (n-0) on that parameter...
-# 0 is biased, 1 is unbiased
-var1 = np.var(x, ddof=1)
-var2 = (1 / (n - 1)) * np.sum((x - mean1) ** 2)
+print(popmean)
+print(sampmean)
 
-print(var1, var2)
+nExpers = 10000
+
+sampleMeans = np.zeros(nExpers)
+
+for i in range(nExpers):
+    sample = np.random.choice(x,size=5,replace=True)
+    sampleMeans[i] = np.mean(sample)
+
+plt.hist(sampleMeans,bins=40,density=True)
+plt.plot([popmean,popmean], [0,.3], 'm--')
+plt.ylabel('Count')
+plt.xlabel('Sample mean')
+plt.show()
