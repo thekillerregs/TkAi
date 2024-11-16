dfb8ba480351d4a84d382e6f7651aed6af8b8298
thekillerregs <thekillerregs@gmail.com>
2024-11-16 12:39:19 -0300

    R: wip

---
+++ R/tk.R
@@ -0,0 +1,12 @@
+# Data preprocessing
+# Importing dataset
+dataset = read.csv(resource_path('Data.csv'))
+
+
+
+
+resource_path <- function(filename) {
+  script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+  base_dir <- normalizePath(file.path(script_dir, ".."))
+  file.path(base_dir, "resources", filename)
+}
