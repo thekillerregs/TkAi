4d344dbbb714b9b267a5d8a741e12c4c496c2ec8
thekillerregs <thekillerregs@gmail.com>
2024-11-17 16:49:55 -0300

    R: Manual Backward Elimination

--- R/tk.R
+++ R/tk.R
@@ -30,3 +30,26 @@ summary(regressor)
 
 #Predicting the test set results
 y_pred = predict(regressor, newdata = test_set)
+
+#Building the optimal model using Backward Elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State, 
+               data = dataset)
+
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend, 
+               data = dataset)
+
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend, 
+               data = dataset)
+
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend, 
+               data = dataset)
+
+summary(regressor)
+
+
