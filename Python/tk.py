bc8c9b805de8b0b60b7ae52a06ff6f8c8f79d02e
thekillerregs <thekillerregs@gmail.com>
2024-11-17 16:13:35 -0300

    Python: Multiple Linear Regression

--- Python/tk.py
+++ Python/tk.py
@@ -1,8 +1,10 @@
-import numpy as np
 import os
-import matplotlib.pyplot as plt
+
+import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
 
 
@@ -13,32 +15,27 @@ def resource_path(filename: str) -> str:
 
 
 # Importing data
-dataset = pd.read_csv(resource_path('Salary_Data.csv'))
+dataset = pd.read_csv(resource_path('50_Startups.csv'))
 
 # Creating datasets
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1]
 
+# Encoding categorical data
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
 # Splitting the datasets into training and test
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-# Linear Regression
+# Multi linear regression
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
+
+# Printing the predictions
 y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.values.reshape(len(y_test), 1)), 1))
 
-# Training set results
-plt.scatter(x_train, y_train, color='red')
-plt.plot(x_train, regressor.predict(x_train), color='blue')
-plt.title('Salary vs Experience (Training Set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
-
-# Test set results
-plt.scatter(x_test, y_test, color='red')
-plt.plot(x_train, regressor.predict(x_train), color='blue')
-plt.title('Salary vs Experience (Test Set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+# Predicting a single value
+print(regressor.predict([[1, 0, 0, 160000, 130000, 300000]]))
