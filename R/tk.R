0998bc96809fb9d71c0505d442077b58714f5bef
thekillerregs <thekillerregs@gmail.com>
2024-11-17 16:27:45 -0300

    R: Multiple Linear Regression

--- R/tk.R
+++ R/tk.R
@@ -6,41 +6,27 @@ resource_path <- function(filename) {
 
 # Data preprocessing
 # Importing dataset
-dataset = read.csv(resource_path('Salary_Data.csv'))
+dataset = read.csv(resource_path('50_Startups.csv'))
+
+# Encoding categorical data
+dataset$State = factor(
+  dataset$State,
+  levels = c('New York', 'California', 'Florida'),
+  labels = c(1, 2, 3)
+)
+
 
 #Splitting the dataset into test and training
 set.seed(123)
-split = sample.split(dataset$Salary, SplitRatio = 2/3)
-
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 #Linear Regression
-regressor=lm(formula = Salary ~ YearsExperience,
-              data = training_set)
+regressor = lm(formula = Profit ~ ., data = training_set)
 
 #Summary of statistical significance
 summary(regressor)
 
 #Predicting the test set results
 y_pred = predict(regressor, newdata = test_set)
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x=training_set$YearsExperience, y=training_set$Salary),
-             colour = 'red') +
-  geom_line(aes(x=training_set$YearsExperience, y= predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Training Set)') +
-  xlab('Years of Experience') +
-  ylab('Salary')
-
-ggplot() +
-  geom_point(aes(x=test_set$YearsExperience, y=test_set$Salary),
-             colour = 'red') +
-  geom_line(aes(x=training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
-            colour = 'blue') +
-  ggtitle('Salary vs Experience (Test Set)') +
-  xlab('Years of Experience') +
-  ylab('Salary')
-
-             
