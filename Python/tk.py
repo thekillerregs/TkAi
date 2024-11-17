f09d273da46a72026d437cf041a00cbfcc5884a4
thekillerregs <thekillerregs@gmail.com>
2024-11-17 14:05:39 -0300

    Python: Linear Regression

--- Python/tk.py
+++ Python/tk.py
@@ -2,62 +2,43 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.impute import SimpleImputer
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
 
-def resource_path(filename):
+def resource_path(filename: str) -> str:
     """Return the absolute path to a file in the 'resources' folder."""
     base_dir = os.path.dirname(os.path.dirname(__file__))  # Parent directory of the Python folder
     return os.path.join(base_dir, 'resources', filename)
 
 
 # Importing data
-dataset = pd.read_csv(resource_path('Data.csv'))
+dataset = pd.read_csv(resource_path('Salary_Data.csv'))
 
 # Creating datasets
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1]
 
-print(x)
-print(y)
-
-# Accounting for missing values
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(x[:, 1:3])
-
-x[:, 1:3] = imputer.transform(x[:, 1:3])
-
-print(x)
-
-# Encoding categories
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-x = np.array(ct.fit_transform(x))
-
-print(x)
-
-# Encoding yes/no labels
-le = LabelEncoder()
-y = le.fit_transform(y)
-
-print(y)
-
 # Splitting the datasets into training and test
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-print(x_train)
-print(x_test)
-print(y_train)
-print(y_test)
+# Linear Regression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
 
-# Feature Scaling - Standardization
-sc = StandardScaler()
-x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
-x_test[:, 3:] = sc.transform(x_test[:, 3:])
+# Training set results
+plt.scatter(x_train, y_train, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
-print(x_train)
-print(x_test)
+# Test set results
+plt.scatter(x_test, y_test, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
