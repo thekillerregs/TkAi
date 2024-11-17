6d2c1c002c6c73d34e6833f20f6dfc7ff9fe796f
thekillerregs <thekillerregs@gmail.com>
2024-11-16 23:58:22 -0300

    R: Dataset, Missing values, Enconding Categories/Labels, Splitting Dataset, Feature Scaling

--- R/tk.R
+++ R/tk.R
@@ -1,12 +1,40 @@
-# Data preprocessing
-# Importing dataset
-dataset = read.csv(resource_path('Data.csv'))
-
-
-
-
 resource_path <- function(filename) {
   script_dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
   base_dir <- normalizePath(file.path(script_dir, ".."))
   file.path(base_dir, "resources", filename)
 }
+
+# Data preprocessing
+# Importing dataset
+dataset = read.csv(resource_path('Data.csv'))
+
+#Accounting for missing values
+dataset$Age = ifelse(is.na(dataset$Age), 
+                     ave(dataset$Age, FUN = function(x) mean (x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary), 
+                     ave(dataset$Salary, FUN = function(x) mean (x, na.rm = TRUE)),
+                     dataset$Salary)
+
+#Enconding Countries into numbers
+dataset$Country = factor(dataset$Country, 
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1,2,3))
+
+#Encoding boolean into 0/1
+dataset$Purchased = factor(dataset$Purchased, 
+                         levels = c('No', 'Yes'),
+                         labels = c(0,1))
+
+#Splitting the dataset into test and training
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Feature Scaling
+training_set[,2:3] = scale(training_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
