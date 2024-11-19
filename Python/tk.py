96e2d7fa1d5788b63fb9d5aa5812238a15391646
thekillerregs <thekillerregs@gmail.com>
2024-11-19 12:40:22 -0300

    Python: Model Selection [WIP]

--- Python/tk.py
+++ Python/tk.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
 
 
 def resource_path(filename: str) -> str:
@@ -13,27 +18,38 @@ def resource_path(filename: str) -> str:
 
 
 # Importing data
-dataset = pd.read_csv(resource_path('Position_Salaries.csv'))
+dataset = pd.read_csv(resource_path('Model_Selection.csv'))
 
 # Creating datasets
 x = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Multiple Linear Regression
+lin_regressor = LinearRegression()
+lin_regressor.fit(x_train, y_train)
+
+# Polynomial Regression
+poly_reg = PolynomialFeatures(degree=4)
+poly_reg.fit(x_train, y_train)
+
+# Support Vector Regression
+svr_train_y = y_train.reshape(len(y_train), 1)
+svr_test_y = y_test.reshape(len(y_test), 1)
+
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+svr_x_train = sc_x.fit_transform(x_train)
+svr_y_train = sc_y.fit_transform(svr_train_y)
+
+svr_reg = SVR(kernel='rbf')
+svr_reg.fit(svr_x_train, svr_y_train)
+
+# Decision Tree Regression
+tree_reg = DecisionTreeRegressor(random_state=0)
+tree_reg.fit(x_train, y_train)
+
 # Random Forest Regression
-regressor = RandomForestRegressor(n_estimators=10, random_state=0)
-regressor.fit(x, y)
-
-# Predicting Salary
-pred_y = regressor.predict([[6.5]])
-
-print(pred_y)
-
-# Visualizing
-x_grid = np.arange(min(x[:, 0]), max(x[:, 0]), 0.1)
-x_grid = x_grid.reshape(len(x_grid), 1)
-plt.scatter(x, y, color='red')
-plt.plot(x_grid, regressor.predict(x_grid), color='blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
-plt.show()
+rf_regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+rf_regressor.fit(x_train, y_train)
