4b20c3c9832d7b73c0e71ac8211695787ce4d1cb
thekillerregs <thekillerregs@gmail.com>
2024-11-19 20:09:47 -0300

    Python: Model Evaluation

--- Python/tk.py
+++ Python/tk.py
@@ -1,10 +1,9 @@
 import os
 
-import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.svm import SVR
@@ -27,12 +26,14 @@ y = dataset.iloc[:, -1].values
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Multiple Linear Regression
-lin_regressor = LinearRegression()
-lin_regressor.fit(x_train, y_train)
+lin_reg = LinearRegression()
+lin_reg.fit(x_train, y_train)
 
 # Polynomial Regression
-poly_reg = PolynomialFeatures(degree=4)
-poly_reg.fit(x_train, y_train)
+poly_ft = PolynomialFeatures(degree=4)
+x_poly = poly_ft.fit_transform(x_train)
+poly_reg = LinearRegression()
+poly_reg.fit(x_poly, y_train)
 
 # Support Vector Regression
 svr_train_y = y_train.reshape(len(y_train), 1)
@@ -41,6 +42,7 @@ svr_test_y = y_test.reshape(len(y_test), 1)
 sc_x = StandardScaler()
 sc_y = StandardScaler()
 svr_x_train = sc_x.fit_transform(x_train)
+svr_x_test = sc_x.transform(x_test)
 svr_y_train = sc_y.fit_transform(svr_train_y)
 
 svr_reg = SVR(kernel='rbf')
@@ -51,5 +53,20 @@ tree_reg = DecisionTreeRegressor(random_state=0)
 tree_reg.fit(x_train, y_train)
 
 # Random Forest Regression
-rf_regressor = RandomForestRegressor(n_estimators=10, random_state=0)
-rf_regressor.fit(x_train, y_train)
+rf_reg = RandomForestRegressor(n_estimators=10, random_state=0)
+rf_reg.fit(x_train, y_train)
+
+# Predictions
+lin_pred = lin_reg.predict(x_test)
+poly_pred = poly_reg.predict(poly_ft.transform(x_test))
+svr_pred = sc_y.inverse_transform(svr_reg.predict(svr_x_test).reshape(-1, 1))
+tree_pred = tree_reg.predict(x_test)
+rf_pred = rf_reg.predict(x_test)
+
+lin_score = r2_score(y_test, lin_pred)
+poly_score = r2_score(y_test, poly_pred)
+svr_score = r2_score(y_test, svr_pred)
+tree_score = r2_score(y_test, tree_pred)
+rf_score = r2_score(y_test, rf_pred)
+
+print(lin_score, poly_score, svr_score, tree_score, rf_score)
