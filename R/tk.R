694e28aedfb27f032bffaf4e96f3b4a06e31db69
thekillerregs <thekillerregs@gmail.com>
2024-11-30 14:42:36 -0300

    R: K-Nearest Neighbors (KNN)

--- R/tk.R
+++ R/tk.R
@@ -8,10 +8,10 @@ resource_path <- function(filename) {
 # Importing dataset
 dataset = read.csv(resource_path('Social_Network_Ads.csv'))
 
-#Encoding the target feature as factor
-dataset$Purchased = factor(dataset$Purchased, levels = c(0,1))
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
 
-# Splititng dataset
+# Splitting dataset
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.75)
@@ -19,15 +19,18 @@ training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[, 1:2] = scale(training_set[, 1:2])
-test_set[, 1:2] = scale(test_set[, 1:2])
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
 
-# Logistic Regression
-classifier = glm(Purchased ~ ., family = binomial, data = training_set)
+# K-NN Regression
+library(class)
+y_pred = knn(
+  train = training_set[, -3],
+  test = test_set[, -3],
+  cl = training_set[, 3],
+  k = 5
+)
 
-# Predicting the Test set Results
-prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
 
 # Creating Confusion Matrix
 cm = table(test_set[, 3], y_pred)
@@ -35,14 +38,18 @@ cm = table(test_set[, 3], y_pred)
 # Visualizing
 library(ElemStatLearn)
 
-set = training_set
+set = test_set
 
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+y_grid = knn(
+  train = training_set[, -3],
+  test = grid_set,
+  cl = training_set[, 3],
+  k = 5
+)
 plot(
   set[, -3],
   main = 'Logistic Regression (Training Set)',
