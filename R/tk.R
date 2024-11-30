bcf44af1cb44a6d7c619d1304f766ac3d555583a
thekillerregs <thekillerregs@gmail.com>
2024-11-30 17:15:32 -0300

    R: Using RBF Kernel in SVM

--- R/tk.R
+++ R/tk.R
@@ -28,7 +28,7 @@ classifier = svm(
   formula = Purchased ~ .,
   data = training_set,
   type = 'C-classification',
-  kernel = 'linear'
+  kernel = 'radial'
 )
 
 y_pred = predict(classifier, newdata = test_set)
