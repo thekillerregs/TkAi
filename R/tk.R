4d56939b2bc85aa6ee1e7d0ff2e301a163bfdfe7
thekillerregs <thekillerregs@gmail.com>
2024-12-01 13:50:58 -0300

    R: K-Means Clustering

--- R/tk.R
+++ R/tk.R
@@ -6,53 +6,40 @@ resource_path <- function(filename) {
 
 # Data preprocessing
 # Importing dataset
-dataset = read.csv(resource_path('Social_Network_Ads.csv'))
+dataset = read.csv(resource_path('Mall_Customers.csv'))
+X <- dataset[4:5]
 
-# Encoding the target feature as factor
-dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
-
-# Splitting dataset
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.75)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
-# Feature Scaling
-training_set[-3] = scale(training_set[-3])
-test_set[-3] = scale(test_set[-3])
-
-# SVM Regression
-library(randomForest)
-classifier = randomForest(x = training_set[-3],
-                          y = training_set$Purchased,
-                          ntree = 10)
-
-y_pred = predict(classifier, newdata = test_set[-3], type='class')
-
-# Creating Confusion Matrix
-cm = table(test_set[, 3], y_pred)
-
-# Visualizing
-library(ElemStatLearn)
-
-set = test_set
-
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
-grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type='class')
+# Elbow Method
+set.seed(6)
+wcss <- vector()
+for (i in 1:10)
+  wcss[i] <- sum(kmeans(X, i)$withinss)
 plot(
-  set[, -3],
-  main = 'Graph',
-  xlab = 'Age',
-  ylab = 'Estimated Salary',
-  xlim = range(X1),
-  ylim = range(X2)
+  1:10,
+  wcss,
+  type = 'b',
+  main = paste('Clusters of clients'),
+  xlab = 'Number of clusters',
+  ylab = 'WCSS'
 )
-contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set,
-       pch = '.',
-       col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
+# Applying K-Means Clustering
+set.seed(29)
+kmeans <- kmeans(X, 5, iter.max = 300, nstart = 10)
+
+# Visualizing the clusters
+library(cluster)
+clusplot(
+  X,
+  kmeans$cluster,
+  lines = 0,
+  shade = TRUE,
+  color = TRUE,
+  labels = 2,
+  plotchar = FALSE,
+  span = TRUE,
+  main = paste('Clusters of clients'),
+  xlab = 'Number of clusters',
+  ylab = 'WCSS'
+)
