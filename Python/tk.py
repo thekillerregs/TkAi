4656b4e90f562a38ce80d0ad404f7543faddf00c
thekillerregs <thekillerregs@gmail.com>
2024-12-01 16:16:34 -0300

    Python: Eclat Association

--- Python/tk.py
+++ Python/tk.py
@@ -19,7 +19,9 @@ transactions = []
 for i in range(0, 7501):
     transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
-rules = apriori(transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
+# Eclat
+rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2,
+                max_length=2)
 results = list(rules)
 print(results)
 
@@ -28,14 +30,11 @@ def inspect(results):
     lhs = [tuple(result[2][0][0])[0] for result in results]
     rhs = [tuple(result[2][0][1])[0] for result in results]
     supports = [result[1] for result in results]
-    confidences = [result[2][0][2] for result in results]
-    lifts = [result[2][0][3] for result in results]
-    return list(zip(lhs, rhs, supports, confidences, lifts))
+    return list(zip(lhs, rhs, supports))
 
 
-resultsinDataFrame = pd.DataFrame(inspect(results),
-                                  columns=['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+resultsinDataFrame = pd.DataFrame(inspect(results), columns=['Product 1', 'Product 2', 'Support'])
 
-resultsinDataFrame = resultsinDataFrame.nlargest(n=10, columns='Lift')
+resultsinDataFrame = resultsinDataFrame.nlargest(n=10, columns='Support')
 
 print(resultsinDataFrame)
