366a87d54fdba9012f4f9efbd3be35db6b65c28a
thekillerregs <thekillerregs@gmail.com>
2024-12-01 14:30:23 -0300

    Python: Hierarchical Clustering

--- Python/tk.py
+++ Python/tk.py
@@ -1,13 +1,9 @@
 import os
 
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
-from sklearn.metrics import confusion_matrix, accuracy_score
-from matplotlib.colors import ListedColormap
+from sklearn.cluster import AgglomerativeClustering
+import scipy.cluster.hierarchy as sch
 
 
 def resource_path(filename: str) -> str:
@@ -22,29 +18,21 @@ dataset = pd.read_csv(resource_path('Mall_Customers.csv'))
 # Creating datasets
 x = dataset.iloc[:, [3, 4]].values
 
-# Evaluating appropriate number of clusters
-wcss = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
-    kmeans.fit(x)
-    wcss.append(kmeans.inertia_)
-
-plt.plot(range(1, 11), wcss)
-plt.title('The Elbow Method')
-plt.xlabel('Number of Clusters')
-plt.ylabel('WCSS')
+# Using dendrogram to evaluate number of clusters
+dendrogram = sch.dendrogram(sch.linkage(x, method='ward'))
+plt.title('Dendogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distance')
 plt.show()
 
-kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
-y_kmeans = kmeans.fit_predict(x)
-print(y_kmeans)
+hc = AgglomerativeClustering(n_clusters=5, metric='euclidean', linkage='ward')
+y_hc = hc.fit_predict(x)
 
-plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
-plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
-plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
-plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s=100, c='yellow', label='Cluster 4')
-plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s=100, c='pink', label='Cluster 5')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='gray', label='Centroids')
+plt.scatter(x[y_hc == 0, 0], x[y_hc == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(x[y_hc == 1, 0], x[y_hc == 1, 1], s=100, c='blue', label='Cluster 2')
+plt.scatter(x[y_hc == 2, 0], x[y_hc == 2, 1], s=100, c='green', label='Cluster 3')
+plt.scatter(x[y_hc == 3, 0], x[y_hc == 3, 1], s=100, c='cyan', label='Cluster 4')
+plt.scatter(x[y_hc == 4, 0], x[y_hc == 4, 1], s=100, c='magenta', label='Cluster 5')
 plt.title('Clusters of Customers')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (0-100)')
