604d4bc541647f0800f01833fe84b4bd9f80c1a3
thekillerregs <thekillerregs@gmail.com>
2024-12-01 15:31:14 -0300

    Python: Apriori Association

--- Python/tk.py
+++ Python/tk.py
@@ -2,8 +2,7 @@ import os
 
 import pandas as pd
 from matplotlib import pyplot as plt
-from sklearn.cluster import AgglomerativeClustering
-import scipy.cluster.hierarchy as sch
+from apyori import apriori
 
 
 def resource_path(filename: str) -> str:
@@ -13,28 +12,30 @@ def resource_path(filename: str) -> str:
 
 
 # Importing data
-dataset = pd.read_csv(resource_path('Mall_Customers.csv'))
+dataset = pd.read_csv(resource_path('Market_Basket_Optimisation.csv'), header=None)
 
 # Creating datasets
-x = dataset.iloc[:, [3, 4]].values
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
-# Using dendrogram to evaluate number of clusters
-dendrogram = sch.dendrogram(sch.linkage(x, method='ward'))
-plt.title('Dendogram')
-plt.xlabel('Customers')
-plt.ylabel('Euclidean Distance')
-plt.show()
+rules = apriori(transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
+results = list(rules)
+print(results)
 
-hc = AgglomerativeClustering(n_clusters=5, metric='euclidean', linkage='ward')
-y_hc = hc.fit_predict(x)
 
-plt.scatter(x[y_hc == 0, 0], x[y_hc == 0, 1], s=100, c='red', label='Cluster 1')
-plt.scatter(x[y_hc == 1, 0], x[y_hc == 1, 1], s=100, c='blue', label='Cluster 2')
-plt.scatter(x[y_hc == 2, 0], x[y_hc == 2, 1], s=100, c='green', label='Cluster 3')
-plt.scatter(x[y_hc == 3, 0], x[y_hc == 3, 1], s=100, c='cyan', label='Cluster 4')
-plt.scatter(x[y_hc == 4, 0], x[y_hc == 4, 1], s=100, c='magenta', label='Cluster 5')
-plt.title('Clusters of Customers')
-plt.xlabel('Annual Income (k$)')
-plt.ylabel('Spending Score (0-100)')
-plt.legend()
-plt.show()
+def inspect(results):
+    lhs = [tuple(result[2][0][0])[0] for result in results]
+    rhs = [tuple(result[2][0][1])[0] for result in results]
+    supports = [result[1] for result in results]
+    confidences = [result[2][0][2] for result in results]
+    lifts = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+
+
+resultsinDataFrame = pd.DataFrame(inspect(results),
+                                  columns=['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
+
+resultsinDataFrame = resultsinDataFrame.nlargest(n=10, columns='Lift')
+
+print(resultsinDataFrame)
