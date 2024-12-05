ced4c034f76785b274e0fc25c88501ac8900162c
thekillerregs <thekillerregs@gmail.com>
2024-12-05 20:46:28 -0300

    Python: Upper Confidence Bound (UCB)

--- Python/tk.py
+++ Python/tk.py
@@ -1,8 +1,9 @@
 import os
 
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
-from apyori import apriori
+import math
 
 
 def resource_path(filename: str) -> str:
@@ -12,29 +13,40 @@ def resource_path(filename: str) -> str:
 
 
 # Importing data
-dataset = pd.read_csv(resource_path('Market_Basket_Optimisation.csv'), header=None)
+dataset = pd.read_csv(resource_path('Ads_CTR_Optimisation.csv'))
 
-# Creating datasets
-transactions = []
-for i in range(0, 7501):
-    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+# Implementing  UCB
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+total_reward = 0
 
-# Eclat
-rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2,
-                max_length=2)
-results = list(rules)
-print(results)
+for n in range(0, N):
+    ad = 0
+    max_upper_bound = 0
+    for i in range(0, d):
+        if numbers_of_selections[i] > 0:
+            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+            delta_i = math.sqrt(3 / 2 * math.log(n + 1) / numbers_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
 
+    ads_selected.append(ad)
+    numbers_of_selections[ad] += 1
+    reward = dataset.values[n, ad]
+    sums_of_rewards[ad] += reward
+    total_reward += reward
 
-def inspect(results):
-    lhs = [tuple(result[2][0][0])[0] for result in results]
-    rhs = [tuple(result[2][0][1])[0] for result in results]
-    supports = [result[1] for result in results]
-    return list(zip(lhs, rhs, supports))
-
-
-resultsinDataFrame = pd.DataFrame(inspect(results), columns=['Product 1', 'Product 2', 'Support'])
-
-resultsinDataFrame = resultsinDataFrame.nlargest(n=10, columns='Support')
-
-print(resultsinDataFrame)
+# Visualizing
+plt.hist(ads_selected)
+plt.title('Histogram of Ads Selections')
+plt.xlabel('Ads')
+plt.xlim(1)
+plt.ylabel('Times Selected')
+plt.show()
