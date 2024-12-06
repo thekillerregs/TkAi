bf20bde2e2821e21aef878eb10dd5c5cc2c84902
thekillerregs <thekillerregs@gmail.com>
2024-12-05 21:31:02 -0300

    Python: Thompson Sampling

--- Python/tk.py
+++ Python/tk.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import math
+import random
 
 
 def resource_path(filename: str) -> str:
@@ -15,33 +16,28 @@ def resource_path(filename: str) -> str:
 # Importing data
 dataset = pd.read_csv(resource_path('Ads_CTR_Optimisation.csv'))
 
-# Implementing  UCB
+# Implementing Thompson Sampling
 N = 10000
 d = 10
 ads_selected = []
-numbers_of_selections = [0] * d
-sums_of_rewards = [0] * d
+numbers_of_rewards_1 = [0] * d
+numbers_of_rewards_0 = [0] * d
 total_reward = 0
-
 for n in range(0, N):
     ad = 0
-    max_upper_bound = 0
+    max_random = 0
     for i in range(0, d):
-        if numbers_of_selections[i] > 0:
-            average_reward = sums_of_rewards[i] / numbers_of_selections[i]
-            delta_i = math.sqrt(3 / 2 * math.log(n + 1) / numbers_of_selections[i])
-            upper_bound = average_reward + delta_i
-        else:
-            upper_bound = 1e400
-        if upper_bound > max_upper_bound:
-            max_upper_bound = upper_bound
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
             ad = i
-
     ads_selected.append(ad)
-    numbers_of_selections[ad] += 1
     reward = dataset.values[n, ad]
-    sums_of_rewards[ad] += reward
-    total_reward += reward
+    if reward == 1:
+        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
+    else:
+        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
+    total_reward = total_reward + reward
 
 # Visualizing
 plt.hist(ads_selected)
