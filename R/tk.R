02110766a18ebfd5c5be2a6148d6e480d3274e6d
thekillerregs <thekillerregs@gmail.com>
2024-12-05 21:03:29 -0300

    R: Upper Confidence Bound (UCB)

--- R/tk.R
+++ R/tk.R
@@ -5,18 +5,39 @@ resource_path <- function(filename) {
 }
 
 # Data preprocessing
-# Importing dataset
-# Creating Matrix
-library(arules)
-dataset = read.transactions(
-  resource_path('Market_Basket_Optimisation.csv'),
-  sep = ',',
-  rm.duplicates = TRUE
+dataset = read.csv(resource_path('Ads_CTR_Optimisation.csv'))
+
+# UCB
+N = 10000
+d = 10
+ads_selected = integer(0)
+numbers_of_selections = integer(d)
+sums_of_rewards = integer(d)
+for (n in 1:N) {
+  ad = 0
+  max_upper_bound = 0
+  for (i in 1:d) {
+    if (numbers_of_selections[i] > 0) {
+      average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+      delta_i = sqrt(3 / 2 * log(n) / numbers_of_selections[i])
+      upper_bound = average_reward + delta_i
+    } else {
+      upper_bound = 1e400
+    }
+    if (upper_bound > max_upper_bound) {
+      max_upper_bound = upper_bound
+      ad = i
+    }
+  }
+  ads_selected = append(ads_selected, ad)
+  numbers_of_selections[ad] = numbers_of_selections[ad] + 1
+}
+
+# Visualizing the results - Histogram
+hist(
+  ads_selected,
+  col = 'blue',
+  main = 'Histogram of Ads Selections',
+  xlab = 'Ads',
+  ylab = 'Number of Selections'
 )
-summary(dataset)
-
-itemFrequencyPlot(dataset, topN = 10)
-
-# Training Eclat Model
-rules = eclat(dataset, parameter = list(support = 0.004, minlen = 2))
-inspect(sort(rules, by = 'support')[1:10])
