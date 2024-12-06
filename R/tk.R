578e9d9a21cd9afb6cd6631064d0d004031e9699
thekillerregs <thekillerregs@gmail.com>
2024-12-06 17:50:36 -0300

    R: Natural Language Processing (NLP)

--- R/tk.R
+++ R/tk.R
@@ -4,43 +4,48 @@ resource_path <- function(filename) {
   file.path(base_dir, "resources", filename)
 }
 
-# Data preprocessing
-dataset = read.csv(resource_path('Ads_CTR_Optimisation.csv'))
+# Importing the dataset
+dataset_original = read.delim(resource_path('Restaurant_Reviews.tsv'), quote = '', stringsAsFactors = FALSE)
 
-# Implementing Thompson Sampling
-N = 10000
-d = 10
-ads_selected = integer(0)
-numbers_of_rewards_1 = integer(d)
-numbers_of_rewards_0 = integer(d)
-total_reward = 0
-for (n in 1:N) {
-  ad = 0
-  max_random = 0
-  for (i in 1:d) {
-    random_beta = rbeta(n = 1,
-                        shape1 = numbers_of_rewards_1[i] + 1,
-                        shape2 = numbers_of_rewards_0[i] + 1)
-    if (random_beta > max_random) {
-      max_random = random_beta
-      ad = i
-    }
-  }
-  ads_selected = append(ads_selected, ad)
-  reward = dataset[n, ad]
-  if (reward == 1) {
-    numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
-  } else {
-    numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
-  }
-  total_reward = total_reward + reward
-}
+# Cleaning the texts
+# install.packages('tm')
+# install.packages('SnowballC')
+library(tm)
+library(SnowballC)
+corpus = VCorpus(VectorSource(dataset_original$Review))
+corpus = tm_map(corpus, content_transformer(tolower))
+corpus = tm_map(corpus, removeNumbers)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords())
+corpus = tm_map(corpus, stemDocument)
+corpus = tm_map(corpus, stripWhitespace)
 
-# Visualizing the results - Histogram
-hist(
-  ads_selected,
-  col = 'blue',
-  main = 'Histogram of Ads Selections',
-  xlab = 'Ads',
-  ylab = 'Number of Selections'
-)
+# Creating the Bag of Words model
+dtm = DocumentTermMatrix(corpus)
+dtm = removeSparseTerms(dtm, 0.999)
+dataset = as.data.frame(as.matrix(dtm))
+dataset$Liked = dataset_original$Liked
+
+# Encoding the target feature as factor
+dataset$Liked = factor(dataset$Liked, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Liked, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Random Forest Classification to the Training set
+# install.packages('randomForest')
+library(randomForest)
+classifier = randomForest(x = training_set[-692],
+                          y = training_set$Liked,
+                          ntree = 10)
+
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-692])
+
+# Making the Confusion Matrix
+cm = table(test_set[, 692], y_pred)
