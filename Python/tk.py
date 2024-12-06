31a383315aca0763cf0e49d207b9cd2320aa2972
thekillerregs <thekillerregs@gmail.com>
2024-12-06 17:50:23 -0300

    Python: Natural Language Processessing (NLP)

--- Python/tk.py
+++ Python/tk.py
@@ -1,12 +1,15 @@
 import os
-
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
 import re
+
 import nltk
+import numpy as np
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 
 def resource_path(filename: str) -> str:
@@ -22,4 +25,26 @@ dataset = pd.read_csv(resource_path('Restaurant_Reviews.tsv'), delimiter='\t', q
 nltk.download('stopwords')
 corpus = []
 for i in range(0, 1000):
-    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i]).lower()
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i]).lower().split()
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    all_stopwords.remove('not')
+    review = [ps.stem(word) for word in review if word not in set(all_stopwords)]
+    review = ' '.join(review)
+    corpus.append(review)
+
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
