3f3cccad038644cd63bd790a91586aa20caeb44b
thekillerregs <thekillerregs@gmail.com>
2024-12-05 22:31:56 -0300

    Python: NLP WIP

--- Python/tk.py
+++ Python/tk.py
@@ -3,8 +3,10 @@ import os
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-import math
-import random
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 
 def resource_path(filename: str) -> str:
@@ -14,35 +16,10 @@ def resource_path(filename: str) -> str:
 
 
 # Importing data
-dataset = pd.read_csv(resource_path('Ads_CTR_Optimisation.csv'))
+dataset = pd.read_csv(resource_path('Restaurant_Reviews.tsv'), delimiter='\t', quoting=3)
 
-# Implementing Thompson Sampling
-N = 10000
-d = 10
-ads_selected = []
-numbers_of_rewards_1 = [0] * d
-numbers_of_rewards_0 = [0] * d
-total_reward = 0
-for n in range(0, N):
-    ad = 0
-    max_random = 0
-    for i in range(0, d):
-        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
-        if random_beta > max_random:
-            max_random = random_beta
-            ad = i
-    ads_selected.append(ad)
-    reward = dataset.values[n, ad]
-    if reward == 1:
-        numbers_of_rewards_1[ad] = numbers_of_rewards_1[ad] + 1
-    else:
-        numbers_of_rewards_0[ad] = numbers_of_rewards_0[ad] + 1
-    total_reward = total_reward + reward
-
-# Visualizing
-plt.hist(ads_selected)
-plt.title('Histogram of Ads Selections')
-plt.xlabel('Ads')
-plt.xlim(1)
-plt.ylabel('Times Selected')
-plt.show()
+# Cleaning texts
+nltk.download('stopwords')
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i]).lower()
