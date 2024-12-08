335f915149f71248b2ff4cbacb6385ba55d8dbf9
thekillerregs <thekillerregs@gmail.com>
2024-12-08 14:30:01 -0300

    Python: Principal Component Analysis (PCA)

--- Python/tk.py
+++ Python/tk.py
@@ -1,9 +1,14 @@
 import os
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
-import tensorflow as tf
+import pandas as pd
 import numpy as np
+from matplotlib.colors import ListedColormap
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 
 def resource_path(filename: str) -> str:
@@ -12,48 +17,44 @@ def resource_path(filename: str) -> str:
     return os.path.join(base_dir, 'resources', filename)
 
 
-# Importing data, Preprocessing
-train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-training_set = train_datagen.flow_from_directory(resource_path('cnn/training_set'), target_size=(64, 64), batch_size=32,
-                                                 class_mode='binary')
+# Importing data
+dataset = pd.read_csv(resource_path('Wine.csv'))
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
-test_set = test_datagen.flow_from_directory(resource_path('cnn/test_set'), target_size=(64, 64), batch_size=32,
-                                            class_mode='binary')
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-cnn = tf.keras.models.Sequential()
-# Convolution
-cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
-# Pooling
-cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
 
-# Second layer
-cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
-cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+# Applying PCA
+pca = PCA(n_components=2)
+x_train = pca.fit_transform(x_train)
+x_test = pca.transform(x_test)
 
-# Flattening
-cnn.add(tf.keras.layers.Flatten())
+# Logistic Regression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(x_train, y_train)
 
-# Full Connection
-cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+# Displaying
+y_pred = classifier.predict(x_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
 
-# Output
-cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
-
-# Compiling
-cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-cnn.fit(x=training_set, validation_data=test_set, epochs=25)
-
-# Single prediction
-test_image = image.load_img(resource_path('cnn/single_prediction/cat_or_dog_1.jpg'), target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
-result = cnn.predict(test_image)
-print(training_set.class_indices)
-if result[0][0] == 1:
-    prediction = 'dog'
-else:
-    prediction = 'cat'
-
-print(prediction)
+x_set, y_set = x_train, y_train
+X1, X2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c=ListedColormap(('red', 'green', 'blue'))(i), label=j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
+plt.legend()
+plt.show()
