93d4219151b9bbe50a54c9194a291d421803a779
thekillerregs <thekillerregs@gmail.com>
2024-12-08 14:32:56 -0300

    R: Principal Component Analysis (PCA)

--- R/tk.R
+++ R/tk.R
@@ -5,56 +5,93 @@ resource_path <- function(filename) {
 }
 
 # Importing the dataset
-dataset = read.csv(resource_path('Churn_Modelling.csv'))
-dataset = dataset[4:14]
+dataset = read.csv(resource_path('Wine.csv'))
 
-# Encoding categorical variable as factors
-dataset$Geography = as.numeric(factor(
-  dataset$Geography,
-  levels = c('France', 'Spain', 'Germany'),
-  labels = c(1, 2, 3)
-))
-
-dataset$Gender = as.numeric(factor(
-  dataset$Gender,
-  levels = c('Female', 'Male'),
-  labels = c(1, 2)
-))
-
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
 library(caTools)
 set.seed(123)
-
-# Split
-
-split = sample.split(dataset$Exited, SplitRatio = 0.8)
+split = sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_set[-11] = scale(training_set[-11])
-test_set[-11] = scale(test_set[-11])
+training_set[-14] = scale(training_set[-14])
+test_set[-14] = scale(test_set[-14])
 
-# Fitting ANN to Training set
-library(h2o)
-h2o.init(nthreads = -1)
+# Applying PCA
+library(caret)
+library(e1071)
+pca = preProcess(x = training_set[-14],
+                 method = 'pca',
+                 pcaComp = 2)
+training_set = predict(pca, training_set)
+training_set = training_set[c(2, 3, 1)]
+test_set = predict(pca, test_set)
+test_set = test_set[c(2, 3, 1)]
 
-
-classifier = h2o.deeplearning(
-  y = 'Exited',
-  training_frame = as.h2o(training_set),
-  activation = 'Rectifier',
-  hidden = c(6, 6),
-  epochs = 100,
-  train_samples_per_iteration = -2
+# Fitting SVM to the Training set
+classifier = svm(
+  formula = Customer_Segment ~ .,
+  data = training_set,
+  type = 'C-classification',
+  kernel = 'linear'
 )
 
-# Predicting
-prob_pred = h2o.predict(classifier, type = 'response', newdata = as.h2o(test_set[-11]))
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
-y_pred = as.vector(y_pred)
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
 
-# Confusion Matrix
-cm = table(test_set[, 11], y_pred)
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
 
-# Disconnecting
-h2o.shutdown()
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(
+  set[, -3],
+  main = 'SVM (Training set)',
+  xlab = 'PC1',
+  ylab = 'PC2',
+  xlim = range(X1),
+  ylim = range(X2)
+)
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set,
+       pch = '.',
+       col = ifelse(
+         y_grid == 2,
+         'deepskyblue',
+         ifelse(y_grid == 1, 'springgreen3', 'tomato')
+       ))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(
+  set[, -3],
+  main = 'SVM (Test set)',
+  xlab = 'PC1',
+  ylab = 'PC2',
+  xlim = range(X1),
+  ylim = range(X2)
+)
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set,
+       pch = '.',
+       col = ifelse(
+         y_grid == 2,
+         'deepskyblue',
+         ifelse(y_grid == 1, 'springgreen3', 'tomato')
+       ))
+points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
